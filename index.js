bddb1ebc1828c41f33389c84d9dd554b7842dbb6
GOURU.KEERTHI REDDY <122141659+Gkeerthi12@users.noreply.github.com>
2024-09-10 23:04:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const playButton = document.querySelector(".banner button");
+    playButton.addEventListener("click", () => {
+        alert("Play button clicked! Add your functionality here.");
+    });
+});
